4569aa00f6c36ea9e8c21e28386d516e27b92d56
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2012-06-09 15:42:23 -0400

    Ultimos cambios Section selector

--- src/com/bspinspector/sectionSelector.java
+++ src/com/bspinspector/sectionSelector.java
@@ -8,6 +8,8 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,6 +23,7 @@ public class sectionSelector extends Activity {
 
 	private String user;
 	private String pass;
+	private String cod_ubicacion;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +34,7 @@ public class sectionSelector extends Activity {
 		Bundle bundle = getIntent().getExtras();
 		this.user = bundle.getString("user");
 		this.pass = bundle.getString("pass");
+		this.cod_ubicacion = bundle.getString("cod_ubicacion");
         
         /*Instancia el DownloadHelper*/
         Downloader dw = new Downloader();
@@ -64,6 +68,7 @@ public class sectionSelector extends Activity {
 	            cabecera.setOrientation(LinearLayout.HORIZONTAL);
 	            cabecera.setBackgroundColor(Color.parseColor("#0A0C29"));
 	            cabecera.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, 40));
+	            cabecera.setPadding(15, 5, 5, 5);
 	            TextView nombre = new TextView(this);
 	            nombre.setText(this.user);
 	            nombre.setTextSize(18);
@@ -71,6 +76,36 @@ public class sectionSelector extends Activity {
 	            nombre.setTypeface(null, Typeface.BOLD);
 	            cabecera.addView(nombre);
 	            ll.addView(cabecera);
+	            
+	            LinearLayout infoSpace = new LinearLayout(this);
+	            infoSpace.setOrientation(LinearLayout.VERTICAL);
+	            infoSpace.setBackgroundColor(Color.parseColor("#CFDCE9"));
+	            infoSpace.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+	            
+	            TextView marca = new TextView(this);
+	            marca.setPadding(15, 3, 3, 3);
+	            marca.setBackgroundColor(Color.parseColor("#FDFDFD"));
+	            marca.setTextColor(Color.parseColor("#838383"));
+	            marca.setTextSize(14);
+	            marca.setTypeface(null, Typeface.BOLD);
+	            marca.setText("MARCA: "+bundle.getString("glosa_marca"));
+	            infoSpace.addView(marca);
+	            
+	            TextView modelo = new TextView(this);
+	            modelo.setPadding(15, 3, 3, 3);
+	            modelo.setBackgroundColor(Color.parseColor("#FDFDFD"));
+	            modelo.setTextColor(Color.parseColor("#838383"));
+	            modelo.setTextSize(14);
+	            modelo.setTypeface(null, Typeface.BOLD);
+	            modelo.setText("MODELO: "+bundle.getString("glosa_modelo"));
+	            infoSpace.addView(modelo);
+	            
+	            ll.addView(infoSpace);
+	            
+	            View bordeinfo = new View(this);
+	            bordeinfo.setBackgroundColor(Color.parseColor("#3F8A00"));
+	            bordeinfo.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, 4));
+	            ll.addView(bordeinfo);
 	            
 	            if (c.moveToFirst()) {
 	                 //Recorremos el cursor hasta que no haya más registros
@@ -105,7 +140,12 @@ public class sectionSelector extends Activity {
 	                	 cont.setOnClickListener(new View.OnClickListener() {
 	                     	public void onClick(View v) {
 	                     		Toast.makeText(sectionSelector.this, temp, Toast.LENGTH_SHORT).show();
-	                     		startActivity(new Intent(sectionSelector.this, formMaker.class).putExtra("sectionId", temp));
+								Intent myIntent = new Intent(sectionSelector.this, formMaker.class);
+								myIntent.putExtra("user", user);
+								myIntent.putExtra("pass", pass);
+								myIntent.putExtra("sectionId", temp);
+								myIntent.putExtra("cod_ubicacion", cod_ubicacion);
+								startActivity(myIntent);
 	                     	}
 	                	 });
 	                	 
@@ -116,9 +156,24 @@ public class sectionSelector extends Activity {
 	                 
 	            }
 	            
-	            View pie = new View(this);
+	            LinearLayout pie = new LinearLayout(this);
+	            pie.setOrientation(LinearLayout.HORIZONTAL);
 	            pie.setBackgroundColor(Color.parseColor("#0A0C29"));
 	            pie.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, 40));
+	            pie.setPadding(15, 5, 5, 5);
+	            
+	            TextView isonline = new TextView(this);
+	            isonline.setPadding(0, 0, 0, 0);
+	            isonline.setTypeface(null, Typeface.BOLD);
+	            if(connectionOK()){
+	            	isonline.setText("ONLINE");
+	            	isonline.setTextColor(Color.parseColor("#00FF00"));
+	            }else{
+	            	isonline.setText("OFFLINE");
+	            	isonline.setTextColor(Color.parseColor("#FF0000"));
+	            }
+	            
+	            pie.addView(isonline);
 	            ll.addView(pie);
 	            
 		        c.close();
@@ -130,5 +185,20 @@ public class sectionSelector extends Activity {
 	        	Log.i("Ups!","El archivo no existe");
 	        }
         
+	}
+    
+	/**
+	 * Verifica Conexion
+	 * */
+	public boolean connectionOK(){
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if(ni!=null && ni.isConnected()){
+			Log.i("INTERNET", "conectado");
+			return true;
+		}else{
+			Log.i("INTERNET", "no conectado");
+			return false;
+		}
 	}
 }
